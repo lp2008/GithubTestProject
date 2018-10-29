eb6ea2708b725bce1e6b10c42a80dd428740c935
Shohan Rahman <shohan.rahman@rmgnetworks.com>
2018-10-29 16:09:37 +0600

    Trending TableView Cell created and Designed.

---
+++ GithubTestProject/GithubTestProject/View/TrendingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrendingTableViewCell.swift
+//  GithubTestProject
+//
+//  Created by Shohan Rahman on 10/29/18.
+//  Copyright © 2018 Shohan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class TrendingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
