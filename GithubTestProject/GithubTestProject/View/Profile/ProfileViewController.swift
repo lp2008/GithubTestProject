04158996f07f557f3b457ecd7318e162c4381115
Shohan Rahman <shohan.rahman@rmgnetworks.com>
2018-10-29 21:13:21 +0600

    Profile Page Design Complete

---
+++ GithubTestProject/GithubTestProject/View/Profile/ProfileViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ProfileViewController.swift
+//  GithubTestProject
+//
+//  Created by Shohan Rahman on 10/29/18.
+//  Copyright © 2018 Shohan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBAction func battleButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func logoutAction(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ GithubTestProject/GithubTestProject/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewModel.swift
+//  GithubTestProject
+//
+//  Created by Shohan Rahman on 10/29/18.
+//  Copyright © 2018 Shohan Rahman. All rights reserved.
+//
+
+import Foundation
+
+class ProfileViewModel {
+    
+}
