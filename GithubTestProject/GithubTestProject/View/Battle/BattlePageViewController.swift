18b629d7668367083b24c1d9bf51118145467a9d
Shohan Rahman <shohan2@gmail.com>
2018-10-30 00:41:04 +0600

    BattlePage View Controller Added.

---
+++ GithubTestProject/GithubTestProject/View/Battle/BattlePageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BattlePageViewController.swift
+//  GithubTestProject
+//
+//  Created by Shohan Rahman on 10/30/18.
+//  Copyright © 2018 Shohan Rahman. All rights reserved.
+//
+
+import UIKit
+
+class BattlePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
