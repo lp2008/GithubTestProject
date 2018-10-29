bb9112159506b6ad0c5867fac4ccd2d4c6818089
Shohan Rahman <shohan2@gmail.com>
2018-10-30 01:26:34 +0600

    BattlePage Design Complete.

---
+++ GithubTestProject/GithubTestProject/ViewModel/BattlePageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BattlePageViewModel.swift
+//  GithubTestProject
+//
+//  Created by Shohan Rahman on 10/30/18.
+//  Copyright © 2018 Shohan Rahman. All rights reserved.
+//
+
+import Foundation
+
+class BattlePageViewModel {
+    
+}
