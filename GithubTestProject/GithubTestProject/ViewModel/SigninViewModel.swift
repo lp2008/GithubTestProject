dcff833f989de908c1c45c5b416012016ce16646
Shohan Rahman <shohan.rahman@rmgnetworks.com>
2018-10-29 18:32:37 +0600

    Signin ViewModel and Signin VIewController class added.

---
+++ GithubTestProject/GithubTestProject/ViewModel/SigninViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SigninViewModel.swift
+//  GithubTestProject
+//
+//  Created by Shohan Rahman on 10/29/18.
+//  Copyright © 2018 Shohan Rahman. All rights reserved.
+//
+
+import Foundation
